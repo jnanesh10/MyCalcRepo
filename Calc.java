7fb11b4000bed1dabcfd1818925025a2b210b96b
jnanesh10 <41481315+jnanesh10@users.noreply.github.com>
2020-10-20 11:18:07 +0530

    prod added

--- Calc.java
+++ Calc.java
@@ -7,11 +7,15 @@ public static int add(int a, int b){
 public static int sub(int a, int b){
   return a-b;
 }
+  
+public static int prod(int a, int b){
+  return a*b;
+}
 
 public static void main(String[] args){
   System.out.println(add(10,20));
   System.out.println(sub(50,40));
-  System.out.println("Hello World");
+  System.out.println(prod(10,20));
 }
 
 }
